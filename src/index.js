49b9402d13fcbd0fb1334a9639734d227de4dbc2
Taras <taras.chernov@mail.ru>
2025-07-26 18:19:39 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  n = n < 0 ? -n : n;
+  const arr = Array.from(String(n));
+  arr.reverse();
+  const reversN = Number(arr.join(''));
+  return reversN;
 };
